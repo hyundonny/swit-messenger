c5d4f7950d5f13bf540e8927523959057bf6752d
Hyun Don Moon <hyundonmoon@gmail.com>
2022-03-12 12:48:35 +0900

    refactor: moved initial messages data to constants folder

--- src/redux/slices/message.js
+++ src/redux/slices/message.js
@@ -1,62 +1,11 @@
 import { createSlice, nanoid } from '@reduxjs/toolkit';
 
 import getSortedData from 'utils/getSortedData';
-import MEMBERS from 'constants/members';
-
-const initialState = [
-  {
-    id: 0,
-    userId: MEMBERS[0].userId,
-    userName: MEMBERS[0].userName,
-    profileImage: MEMBERS[0].profileImage,
-    content: '이거 깃허브에 푸쉬한 커밋 어떻게 되돌리지...? 급한데 아는사람..?',
-    date: '2022-02-11 01:12:32',
-  },
-  {
-    id: 1,
-    userId: MEMBERS[1].userId,
-    userName: MEMBERS[1].userName,
-    profileImage: MEMBERS[1].profileImage,
-    content: '글쎄 🤔',
-    date: '2022-02-11 01:13:02',
-  },
-  {
-    id: 2,
-    userId: MEMBERS[2].userId,
-    userName: MEMBERS[2].userName,
-    profileImage: MEMBERS[2].profileImage,
-    content: '그거 그냥 터미널 열고 `sudo rm -rf /` 쳐봐',
-    date: '2022-02-11 01:14:52',
-  },
-  {
-    id: 3,
-    userId: MEMBERS[1].userId,
-    userName: MEMBERS[1].userName,
-    profileImage: MEMBERS[1].profileImage,
-    content: '응?',
-    date: '2022-02-11 01:15:32',
-  },
-  {
-    id: 4,
-    userId: MEMBERS[2].userId,
-    userName: MEMBERS[2].userName,
-    profileImage: MEMBERS[2].profileImage,
-    content: '아닌가? ㅋㅋㅋㅋ',
-    date: '2022-02-11 01:18:02',
-  },
-  {
-    id: 5,
-    userId: MEMBERS[0].userId,
-    userName: MEMBERS[0].userName,
-    profileImage: MEMBERS[0].profileImage,
-    content: '컴퓨터가 날라갔다는데요 선생님?',
-    date: '2022-02-11 01:20:28',
-  },
-];
+import { initMessages } from 'constants/messages';
 
 export const messageSlice = createSlice({
   name: 'message',
-  initialState,
+  initialState: initMessages,
   reducers: {
     add: {
       reducer: (state, { payload }) => {
@@ -81,6 +30,6 @@ export const messageSlice = createSlice({
     },
   },
 });
-export const { add, remove } = messageSlice.actions;
 
+export const { add, remove } = messageSlice.actions;
 export default messageSlice.reducer;
